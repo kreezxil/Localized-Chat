66461a3fc754acf6c2b69eedb49e3ffd9084faec
roger <roger@192.168.1.100>
2018-04-20 18:08:09 -0500

    phase 5?
    Everything I wanted to keep is ported up now. the red squiggles are
    gone. Now I have to set a server environment to test it. Well I should
    try to run it first and see where it will crash first.
    
    And there are some tweaks that have to be made so the default talk
    command is the same thing that happens when type on the chat line
    without a /

--- src/main/java/com/kreezcraft/localizedchat/TalkCommand.java
+++ src/main/java/com/kreezcraft/localizedchat/TalkCommand.java
@@ -10,17 +10,25 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.gui.PlayerListComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.util.ChunkCoordComparator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
 
 public class TalkCommand extends CommandBase{
     private List aliases;
 
-    public TalkCommand(){
+    public TalkCommand() {
         this.aliases = new ArrayList();
         this.aliases.add("talk");
         this.aliases.add("speak");
@@ -29,124 +37,90 @@ public class TalkCommand extends CommandBase{
     }
 
     @Override
-    public String getCommandName()
-    {
+    public String getName() {
         return "talk";
     }
 
     @Override
-    public String getCommandUsage(ICommandSender icommandsender)
-    {
+    public String getUsage(ICommandSender sender) {
         return "talk <range> <text>";
     }
 
-    @Override
-    public List getCommandAliases()
-    {
-        return this.aliases;
-    }
-
-    @Override
-    public void processCommand(ICommandSender icommandsender, String[] astring)
-    {
-        EntityPlayer player;
-
-        if(icommandsender instanceof EntityPlayer){
-            player = (EntityPlayer)icommandsender;
-
-        }
-        else {
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText("Player only command!"));
-            return;
-        }
-        if(astring.length < 2)
-        {
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText( EnumChatFormatting.DARK_RED + "Invalid arguments."));
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText( EnumChatFormatting.DARK_GREEN + "Use /talk <blockRange> <Message... ...>"));
-            return;
-        }
-
-        double range = 0;
-        try {
-            range = Double.valueOf(astring[0]);
-        } catch (Exception e){
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText( EnumChatFormatting.DARK_RED + "Not a recognised number: " + astring[0]));
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText( EnumChatFormatting.DARK_GREEN + "Use /talk <blockRange> <Message... ...>"));
-            return;
-        }
-        if (range > 50.0){
-            icommandsender.addChatMessage((IChatComponent) new ChatComponentText( EnumChatFormatting.DARK_RED + "Not a chance you can shout as far as 50 blocks"));
-            return;
-        }
-        StringBuilder strBuilder = new StringBuilder();
-        boolean done1stword = false;
-        for (String word : astring){
-            if (done1stword) {
-                strBuilder.append(word).append(" ");
-            }
-            done1stword = true;
-        }
-        String message = strBuilder.toString();
-        System.out.println(message);
-        World workingWorld = icommandsender.getEntityWorld();
-        List playerEntities = workingWorld.playerEntities;
-        EntityPlayer mainPlayer = workingWorld.getPlayerEntityByName(icommandsender.getCommandSenderName());
-        for(Object name : playerEntities){
-            EntityPlayer comparePlayer = (workingWorld.getPlayerEntityByName(((EntityPlayerMP) name).getCommandSenderName()));
-            if (compareCoordinatesDistance(mainPlayer.getPlayerCoordinates(),comparePlayer.getPlayerCoordinates()) <= range){
-                  ((EntityPlayerMP) name).addChatMessage((IChatComponent) new ChatComponentText(EnumChatFormatting.GOLD + "[From " + EnumChatFormatting.RED + compareCoordinatesDistance(mainPlayer.getPlayerCoordinates(),comparePlayer.getPlayerCoordinates()) + EnumChatFormatting.GOLD + " blocks away] " + EnumChatFormatting.GRAY + "<" + icommandsender.getCommandSenderName() + "> " + EnumChatFormatting.WHITE + message));
-             }
-
-        }
-    }
-
-    public double compareCoordinatesDistance(ChunkCoordinates player1, ChunkCoordinates player2){
-        double x = Math.abs(player1.posX - player2.posX);
-        double y = Math.abs(player1.posY - player2.posY);
-        double z = Math.abs(player1.posZ - player2.posZ);
-        return x + y + z;
-    }
-
-    @Override
-    public boolean canCommandSenderUseCommand(ICommandSender icommandsender)
-    {
-        return true;
-    }
-
-    @Override
-    public List addTabCompletionOptions(ICommandSender icommandsender,
-                                        String[] astring)
-    {
-        return null;
-    }
-
-    @Override
-    public boolean isUsernameIndex(String[] astring, int i)
-    {
-        return false;
-    }
-
-    @Override
-    public int compareTo(Object o)
-    {
-        return 0;
-    }
-
 	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	@Nonnull
+	public List<String> getAliases() {
+		return aliases;
+	}
+
+	public double compareCoordinatesDistance(BlockPos player1, BlockPos player2) {
+		double x = Math.abs(player1.getX() - player2.getX());
+		double y = Math.abs(player1.getY() - player2.getY());
+		double z = Math.abs(player1.getZ() - player2.getZ());
+		return x + y + z;
+	}
+
+	@Nonnull
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
+			@Nullable BlockPos targetPos) {
+		return Collections.emptyList();
 	}
 
 	@Override
-	public String getUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+		return true;
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
-		
+	       EntityPlayer player;
+
+	        if(sender instanceof EntityPlayer){
+	            player = (EntityPlayer)sender;
+
+	        }
+	        else {
+	            return;
+	        }
+
+	        if(args.length < 2)
+	        {
+	            player.sendMessage(new TextComponentString( ChatFormatting.DARK_RED + "Invalid arguments."));
+	            player.sendMessage(new TextComponentString( ChatFormatting.DARK_GREEN + "Use /talk <blockRange> <Message... ...>"));
+	            return;
+	        }
+
+	        double range = 0;
+	        try {
+	            range = Double.valueOf(args[0]);
+	        } catch (Exception e){
+	            player.sendMessage(new TextComponentString( ChatFormatting.DARK_RED + "Not a recognised number: " + args[0]));
+	            player.sendMessage(new TextComponentString( ChatFormatting.DARK_GREEN + "Use /talk <blockRange> <Message... ...>"));
+	            return;
+	        }
+	        if (range > 100.0){
+	            player.sendMessage(new TextComponentString( ChatFormatting.DARK_RED + "Not a chance you can only talk as far out as 100 blocks"));
+	            return;
+	        }
+	        StringBuilder strBuilder = new StringBuilder();
+	        boolean done1stword = false;
+	        for (String word : args){
+	            if (done1stword) {
+	                strBuilder.append(word).append(" ");
+	            }
+	            done1stword = true;
+	        }
+	        String message = strBuilder.toString();
+	        System.out.println(message);
+	        World workingWorld = player.getEntityWorld();
+	        List playerEntities = workingWorld.playerEntities;
+	        EntityPlayer mainPlayer = workingWorld.getPlayerEntityByName(player.getName());
+	        for(Object name : playerEntities){
+	            EntityPlayer comparePlayer = (workingWorld.getPlayerEntityByName(((EntityPlayerMP) name).getName()));
+	            if (compareCoordinatesDistance(mainPlayer.getPosition(),comparePlayer.getPosition()) <= range){
+	                  ((EntityPlayerMP) name).sendMessage(new TextComponentString(ChatFormatting.GOLD + "[From " + ChatFormatting.RED + compareCoordinatesDistance(mainPlayer.getPosition(),
+	                		  comparePlayer.getPosition()) + ChatFormatting.GOLD + " blocks away] " + ChatFormatting.GRAY + "<" + player.getName() + "> " + ChatFormatting.WHITE + message));
+	             }
+
+	        }
 	}
 }
